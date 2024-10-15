a7cb4a29776ab0dc2109ddd2854ea80fabe18721
Bre Humphres <breannahumphres@gmail.com>
2024-10-14 18:09:07 -0700

    update and delete work

--- server/app.py
+++ server/app.py
@@ -36,9 +36,11 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
+   
 
 
 api.add_resource(Plants, '/plants')
+       
 
 
 class PlantByID(Resource):
@@ -47,6 +49,31 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        if not plant:
+            return make_response({"error":"Plant not found"}, 404)
+        data = request.json       
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+        
+        db.session.commit()
+
+        response_dict = plant.to_dict()
+        response = make_response(
+            response_dict,
+            200
+        )
+        return response
+    
+    def delete(self,id):
+        plant = Plant.query.filter(Plant.id == id).first()
+        if not plant:
+            return make_response({"error": "Plant not found"},404)
+        
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('',204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
